31ec2caa77ba36528955e74b4fd336d941efb163
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2025-01-26 16:22:11 +0100

    new commit

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,9 @@
 export const getBaseUrl = () => {
-  return "http://localhost:5000", "https://mern-bookstore-khaki.vercel.app";
+  if (process.env.NODE_ENV === "production") {
+    // If in production environment, use the production URL (Vercel)
+    return "https://mern-bookstore-khaki.vercel.app"; // Replace with your actual production URL
+  } else {
+    // If in development environment, use the local server
+    return "http://localhost:5000";
+  }
 };
