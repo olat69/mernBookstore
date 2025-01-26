c80da02ee6fde418f989fa2dd8f0e98f06e825b1
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2025-01-26 17:20:36 +0100

    new commit

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,7 @@
 export const getBaseUrl = () => {
-  return "http://localhost:5000";
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:5000";
+  } else {
+    return "https://mern-bookstore-backend-pearl.vercel.app";
+  }
 };
