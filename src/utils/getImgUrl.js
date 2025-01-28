15d542cafd79d867fbc734c60d694ec472819a3e
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2025-01-28 13:40:53 +0100

    new commit

--- src/utils/getImgUrl.js
+++ src/utils/getImgUrl.js
@@ -1,5 +1,11 @@
 function getImgUrl(name) {
-  return new URL(`../assets/${name}`, import.meta.url);
+  if (!getImgUrl.cache) {
+    getImgUrl.cache = new Map();
+  }
+  if (!getImgUrl.cache.has(name)) {
+    getImgUrl.cache.set(name, new URL(`../assets/${name}`, import.meta.url));
+  }
+  return getImgUrl.cache.get(name);
 }
 
 export { getImgUrl };
